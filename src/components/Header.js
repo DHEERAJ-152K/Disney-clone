f18afe438ee582f1a680b07eb4246d896181cc56
Dheeraj K P <dheerudheeraj033@gmail.com>
2023-12-26 14:54:38 +0530

    Fetch user data from firebase and update on Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import './stylesheets/Header.css'
 import logo from '../images/logo.svg'
-
 import {getAuth,GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 import { firebaseApp } from '../Firebase';
+import { useEffect,useState } from 'react';
+
+
 const Header=(props)=>{
 
-   const signInWithGoogle=async()=>{
-        const auth=getAuth(firebaseApp);
-        const provider=new GoogleAuthProvider();
+    const signInWithGoogle=async()=>{            //Signing-In with firebase's google authentication with popup method.
+    const auth=getAuth(firebaseApp);
+    const provider=new GoogleAuthProvider();
 
         try{
             const result=await signInWithPopup(auth, provider);
@@ -18,39 +20,76 @@ const Header=(props)=>{
         }
     };
 
+    //Fetching data of user and showing it on navbar .
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+        // Update user state when authentication state changes
+        setUser(user);
+        });
+
+        // Cleanup function to unsubscribe when the component unmounts
+        return () => unsubscribe();
+    }, [auth]);
+    
+
     return(
         <div className="header-sec">
-        <div className='toolbar'>
-            <img className='logo' src={logo} alt='logo'/>
+            {!user? (<img className='logo' src={logo} alt='logo'/>):<></>}
+                
+            {user ? (
+                <>
+                    <div className='toolbar'>
+                        <img className='logo' src={logo} alt='logo'/>
 
+                        <span>
+                        <i class="fa-solid fa-house"></i>
+                        <h5>HOME</h5>
+                        </span>
+
+                        <span>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <h5>SEARCH</h5>
+                        </span>
+
+                        <span>
+                        <i class="fa-solid fa-plus"></i>
+                        <h5>WATCHLIST</h5>
+                        </span>
+
+                        <span>
+                        <i class="fa-solid fa-star"></i>
+                        <h5>ORIGINALS</h5>
+                        </span>
+
+                        <span>
+                        <i class="fa-solid fa-tv"></i>
+                        <h5>MOVIES</h5>
+                        </span>
+
+                        <span>
+                        <i class="fa-solid fa-film"></i>
+                        <h5>SERIES</h5>
+                        </span>
+                        
+                    </div>
+
+                    <div className='user-data'>
+                    
+                        <h5>Hey, {user.displayName}</h5>
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                        <img className='profile-picture' src={user.photoURL} alt="Profile" />
+                    </div>
+                </>
+            ) : (
+                // Display login/signup links when not logged in
+                <>
+                    <a onClick={signInWithGoogle}><h5>LOGIN</h5></a>
+                </>
+            )}
             
-                <span>
-                <i class="fa-solid fa-house"></i>
-                <h5>HOME</h5>
-                </span>
-                <span>
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <h5>SEARCH</h5>
-                </span>
-                <span>
-                <i class="fa-solid fa-plus"></i>
-                <h5>WATCHLIST</h5>
-                </span>
-                <span>
-                <i class="fa-solid fa-star"></i>
-                <h5>ORIGINALS</h5>
-                </span>
-                <span>
-                <i class="fa-solid fa-tv"></i>
-                <h5>MOVIES</h5>
-                </span>
-                <span>
-                <i class="fa-solid fa-film"></i>
-                <h5>SERIES</h5>
-                </span>
-            </div>
-
-            <a onClick={signInWithGoogle}><h5>LOGIN</h5></a>
         </div>
     );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,20 +6,23 @@ import ctaLogo2 from '../images/cta-logo-two.png'
 const Login=(props)=>{
     return (
         <div>
-        <Header />
+        
+            <Header /> 
             <div className='container'>
-           <div className='credentials'>
-                <img className='cta-logo' src={ctaLogo} width='100%'/>
-                <button className='btn'><p><b>GET ALL THERE</b></p></button>
-                <p>Get Premier Access to Raya and the Last Dragon for an additional fee
-                    with a Disney+ subscription. As of 03/26/21, the price of Disney+
-                    and The Disney Bundle will increase by $1.
-                </p>
-                <img className='cta-logo2' src={ctaLogo2} width='100%'/>
-            </div>
+                <div className='credentials'>
+                    <img className='cta-logo' src={ctaLogo} width='100%'/>
+                    <button className='btn'><p><b>GET ALL THERE</b></p></button>
 
-           
-        </div>
+                    <p>Get Premier Access to Raya and the Last Dragon for an additional fee
+                        with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                        and The Disney Bundle will increase by $1.
+                    </p>
+
+                    <img className='cta-logo2' src={ctaLogo2} width='100%'/>
+                </div>
+
+            
+            </div>
         </div>
         
     );
